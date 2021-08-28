26343215c3fb2cb1b65afd3bd0f2a61e5f8071db
NicatMirzaev <nicatmirzoev111@gmail.com>
2021-08-28 23:04:20 +0400

    added about page

---
+++ components/pages/About.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import dynamic from 'next/dynamic';
+import ReactMarkdown from 'react-markdown';
+import { useTranslation } from 'react-i18next';
+
+const AboutPage = () => {
+  const remarkGfm = dynamic(() => import('remark-gfm'), { ssr: false });
+  const { t } = useTranslation();
+  return (
+    <div className="flex pt-7 flex-col md:w-8/12 w-full md:pt-7 p-2 h-full mx-auto">
+      <ReactMarkdown
+        components={{
+          a: ({ ...props }) => (
+            <a target="_blank" style={{ color: '#0000EE' }} {...props} />
+          ),
+          p: ({ ...props }) => (
+            <p style={{ marginBottom: '10px' }} {...props} />
+          ),
+        }}
+        remarkPlugins={[remarkGfm]}
+      >
+        {t('about')}
+      </ReactMarkdown>
+    </div>
+  );
+};
+
+export default AboutPage;
---
+++ pages/about.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Layout from '../components/Layout';
+import AboutPage from '../components/pages/About';
+
+const About = () => (
+  <Layout title="About">
+    <AboutPage />
+  </Layout>
+);
+
+export default About;
